bee016d8882afdf0e7eb5b419a759d2e21a95d79
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2024-02-21 10:54:43 +0300

    Dto-Component Start

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -1,17 +1,21 @@
 ﻿using AutoMapper;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.AboutDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
 
 namespace HotelProject.WebUI.Mapping
 {
-    public class AutoMapperConfig:Profile
+    public class AutoMapperConfig : Profile
     {
         public AutoMapperConfig()
         {
             CreateMap<ResultServiceDto, Service>().ReverseMap();
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
             CreateMap<CreateServiceDto, Service>().ReverseMap();
+
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
         }
-       
+
     }
 }
